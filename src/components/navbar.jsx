020386f3afc3cc17fb6c1d8d63499d00be3d4f50
Krishaveni RL <krishnaveni.rl@gmail.com>
2020-12-22 13:19:50 -0600

    NavBar - total counters

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,10 @@ class NavBar extends Component {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="www.google.com">
-          Navbar
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
         </a>
       </nav>
     );
